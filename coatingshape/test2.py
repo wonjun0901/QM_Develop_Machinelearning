aedf2d05029c5ad22ee464c321f52cbe74b4e30c
wonjun0901 <wonjun0901@gmail.com>
2020-02-26 07:34:45 +0900

    200225_집에서 미팅자료 준비

--- coatingshape/test2.py
+++ coatingshape/test2.py
@@ -20,8 +20,8 @@ from skimage.morphology import reconstruction
 
 import matplotlib
 #Data1 = io.imread('coatingshape/x1_side_mid.tif', plugin='tifffile')
-Data1 = io.imread('coatingshape/Control0.5_S33.tif', as_gray=True)
-Data2 = io.imread('coatingshape/Control0.5_S33.tif', as_gray=True)
+Data1 = io.imread('coatingshape/x1.0 S26.tif', as_gray=True)
+Data2 = io.imread('coatingshape/x1.0 S26.tif', as_gray=True)
 #Data1[Data1<120] = 255
 # Data1 = Data1[200:350, :]
 Data3 = gaussian_filter(Data2, 1.5, order=0)
@@ -38,14 +38,14 @@ edges3 = feature.canny(Data2, sigma=1.5)
 
 markers = np.zeros_like(Data2)
 
-markers[Data2 < 170] = 1
+markers[Data2 < 130] = 1
 markers[Data2 > 190] = 2
 
 
 segmentation = morphology.watershed(edges2, markers)
-segmentation[segmentation>=2] = 0
+segmentation[segmentation >= 2] = 0
 
-a1= np.array(segmentation)
+a1 = np.array(segmentation)
 #print(np.count_nonzero(a1 == 1))
 
 segmentation = ndi.binary_fill_holes(segmentation)
@@ -55,8 +55,8 @@ image_label_overlay = label2rgb(labeled_coins, image=Data1)
 
 fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharey=True)
 axes[0].imshow(Data2, cmap=plt.cm.gray, interpolation='nearest')
-#axes[0].contour(segmentation, [0.5], linewidths=2, colors='y')
-axes[1].imshow(edges2, cmap=plt.cm.gray)
+axes[0].contour(segmentation, [0.5], linewidths=2, colors='y')
+axes[1].imshow(image_label_overlay, cmap=plt.cm.gray)
 
 #hyst = filters.apply_hysteresis_threshold(edges2, low, high)
 #a1= np.array(labeled_coins)
@@ -67,13 +67,13 @@ axes[1].imshow(edges2, cmap=plt.cm.gray)
 #print(np.count_nonzero(a1 == 5))
 #print(np.count_nonzero(a1 == 6))
 
-#fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
-#ax1.imshow(Data2, cmap=plt.cm.gray, interpolation='nearest')
-#ax1.axis('off')
-#ax2.imshow(markers, vmin=Data3.min(), vmax=Data3.max(), cmap=plt.cm.gray)
-#ax2.axis('off')
-#ax3.imshow(segmentation, cmap=plt.cm.gray)
-#ax3.axis('off')
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
+ax1.imshow(Data2, cmap=plt.cm.gray, interpolation='nearest')
+ax1.axis('off')
+ax2.imshow(markers, vmin=Data3.min(), vmax=Data3.max(), cmap=plt.cm.gray)
+ax2.axis('off')
+ax3.imshow(segmentation, cmap=plt.cm.gray)
+ax3.axis('off')
 
 plt.show()
 
@@ -81,12 +81,12 @@ plt.show()
 #fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
 #
 #ax1.imshow(edges1, cmap=plt.cm.gray)
-#ax1.axis('off')
+# ax1.axis('off')
 #
 #ax2.imshow(edges2, cmap=plt.cm.gray)
-#ax2.axis('off')
+# ax2.axis('off')
 #
 #ax3.hist(Data1.ravel(), bins=512)
 #
-#fig.tight_layout()
-#plt.show()
+# fig.tight_layout()
+# plt.show()
