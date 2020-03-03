4b9a36bf62f37a094849bb11fc10056fd6e32681
pawj0901 <pawj0901@gmail.com>
2020-03-03 19:04:02 +0900

    flowrate update - rolling mean

--- test/flowrate.py
+++ test/flowrate.py
@@ -6,16 +6,22 @@ import pandas as pd
 
 df = pd.read_excel('C:/Users/wonju/Documents/GitHub/QM_Develop_Machinelearning/test/data_flowrate.xlsx', header=0, index=None)
 print(df)
-time=df['Time'][550:]
+time=df['Time']
 
-flowrate = df['flowrate'][550:]
+flowrate = df['flowrate']
+rolling_mean = flowrate.rolling(window=21).mean()
+
+#flowrate = flowrate - rolling_mean
 flows = np.array(flowrate)
 
 print(np.mean(flows))
 
 
 plt.plot(time, flowrate)
+plt.plot(time, rolling_mean, c='r')
 plt.xlabel('time(s)')
 plt.ylabel('flowrate(ul/min)')
-plt.grid(c='r')
+plt.xticks(np.arange(550, 650, step=10))
+plt.yticks(np.arange(150, 450, step=50))
+plt.grid(which='both', color='gray', linestyle='--', linewidth=0.5)
 plt.show()
