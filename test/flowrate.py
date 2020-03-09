3d04d612f0ecb6625d1e30cbd806409616108e83
wonjun0901 <wonjun0901@gmail.com>
2020-03-10 07:40:37 +0900

    dddd

--- test/flowrate.py
+++ test/flowrate.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 df = pd.read_excel(
     './test/data_flowrate1.xlsx', header=0, index=None)
-#print(df)
+# print(df)
 
 time = df['Time']
 flowrate = df['flowrate']
@@ -14,14 +14,19 @@ flowrate = df['flowrate']
 rolling_mean = flowrate.rolling(window=20, center=True).mean()
 
 rolling_mean = pd.DataFrame(rolling_mean)
-#slope = rolling_mean.diff()
+
+slope = rolling_mean.diff()
+
+pd.set_option('display.max_rows', 150)
+
+print(df)
 #slope = pd.DataFrame(slope)
-#slope.fillna(value=0)
-#print(slope)
+# slope.fillna(value=0)
+# print(slope)
 
 #print(slope.loc[slope['flowrate'] > 10])
 #print(df.loc[df['flowrate'].rolling(window=20, center=True).mean() > 50 ])
-#print(rolling_mean)
+# print(rolling_mean)
 #flowrate = flowrate - rolling_mean
 #flows = np.array(flowrate)
 
