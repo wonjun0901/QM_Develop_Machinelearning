a1deddcb0ca011e6a7b0794baa4d28d269049205
pawj0901 <35714515+wonjun0901@users.noreply.github.com>
2021-02-23 18:02:17 +0900

    commit1
    dasdfasd

--- ELISA_4pl_cf/4pl_cf.py
+++ ELISA_4pl_cf/4pl_cf.py
@@ -13,54 +13,135 @@ def logistic4(x, A, B, C, D):
 
 #df = df.drop_na()
 
-xdata = df['Conc']
-xdata = xdata.dropna()
-#print(xdata)
-ydata = df['Value']
-ydata = ydata.dropna()
-#print(ydata)
-xdata1 = df['Conc1']
-xdata1 = xdata1.dropna()
-#print(xdata1)
-ydata1 = df['Value1']
-ydata1 = ydata1.dropna()
-#print(ydata1)
-#print(ydata1)
-popt, pcov = curve_fit(logistic4, xdata, ydata)
-popt1, pcov1 = curve_fit(logistic4, xdata1, ydata1)
+### standard ####
+std_dup = 2
+dil_num = 5
 
-residuals = ydata - logistic4(xdata, *popt)
+std_xdata = df.iloc[:dil_num, 0:std_dup]
+std_xdata = np.reshape(np.array(std_xdata), -1)
+std_xdata = pd.Series(std_xdata)
+
+std_ydata = df.iloc[:dil_num, 10:10+std_dup]
+std_ydata = np.reshape(np.array(std_ydata), -1)
+std_ydata = pd.Series(std_ydata)
+
+popt, pcov = curve_fit(logistic4, std_xdata, std_ydata, bounds=(0, [10,10,50,4]))
+
+print(popt)
+
+residuals = std_ydata - logistic4(std_xdata, *popt)
 ss_res = np.sum(residuals**2)
 
-ss_tot = np.sum((ydata-np.mean(ydata))**2)
+ss_tot = np.sum((std_ydata-np.mean(std_ydata))**2)
 r_squared = 1 - (ss_res/ss_tot)
 
 print(r_squared)
-#print(popt)
-#print(pcov)
-x_fit = np.linspace(0.01,1000,1000)
+
+x_fit = np.linspace(0.1,100,1000)
 y_fit = logistic4(x_fit, *popt)
-#y_fit
 
-x_fit1 = np.linspace(0.01,1000,1000)
-y_fit1 = logistic4(x_fit1, *popt1)
+plt.plot(std_xdata, std_ydata, 'o', label = 'standard')
+plt.plot(x_fit, y_fit, label = '4pl_curve_fit')
+plt.legend()
+plt.xscale('log')
+plt.show()
 
-plt.plot(xdata, ydata, 'o', label = 'standard')
-plt.plot(x_fit, y_fit, label = '4pl_curve')
+####################################################################
+# 샘플 갯수
+spl_num = 3
 
-plt.plot(xdata1, ydata1, 'o', label = 'data')
-plt.plot(x_fit1, y_fit1, label = 'fit')
+spl_dup = [0, 2, 2,2]
+
+spl_sum = np.cumsum(spl_dup, axis=0)
+
+proc_spl_ydata = []
+
+for i in range(0, spl_num):
+
+    proc_spl_ydata.append(df.iloc[:dil_num, 12+spl_sum[i]:12+spl_sum[i]+spl_dup[i+1]])
+    proc_spl_ydata[i] = np.reshape(np.array(proc_spl_ydata[i]), -1)
+    proc_spl_ydata[i] = proc_spl_ydata[i][np.logical_not(np.isnan(proc_spl_ydata[i]))]
+
+proc_spl_xdata = []
+
+for i in range(0, spl_num):
+
+    proc_spl_xdata.append(df.iloc[:dil_num, 2+spl_sum[i]:2+spl_sum[i]+spl_dup[i+1]])
+    proc_spl_xdata[i] = np.reshape(np.array(proc_spl_xdata[i]), -1)
+    proc_spl_xdata[i] = proc_spl_xdata[i][np.logical_not(np.isnan(proc_spl_xdata[i]))]
 
 
-#plt.show()
-# 역수를 구해서 OD값에서 conc 값 알아내기
+# #print(ydata1)
+# #print(ydata1)
+
+# popt1, pcov1 = curve_fit(logistic4, xdata1, ydata1)
 def solvex(y, A, B, C, D):
     """rearranged 4PL logoistic equation."""
     return C*(((A-D)/(y-D)-1.0)**(1.0/B))
 
-a = solvex(ydata1, *popt)
-print(a)
-plt.plot(a, ydata1, 'o', label='sample')
+regressed_x = []
+
+for i in range(0, spl_num):
+    regressed_x.append(solvex(proc_spl_ydata[i], *popt))
+
+for i in range(0, spl_num):
+    plt.plot(regressed_x[i], proc_spl_ydata[i], 'o', label=i+1)
+
+plt.plot(x_fit, y_fit, label = '4pl_curve_fit')
 plt.legend()
 plt.xscale('log')
 plt.show()
+
+y_fit1 = []
+popt1 = []
+pcov1 = []
+
+popt1, pcov1 = curve_fit(logistic4, proc_spl_xdata[0], proc_spl_ydata[0], bounds=([0,0,0,0], [1,10,15,5]))
+popt2, pcov2 = curve_fit(logistic4, proc_spl_xdata[1], proc_spl_ydata[1], bounds=([0,0,0,0], [1,10,15,5]))
+popt3, pcov3 = curve_fit(logistic4, proc_spl_xdata[2], proc_spl_ydata[2], bounds=([0,0,0,0], [1,10,15,5]))
+
+y_fit1 = []
+y_fit1.append(logistic4(x_fit, *popt1))
+y_fit1.append(logistic4(x_fit, *popt2))
+y_fit1.append(logistic4(x_fit, *popt3))
+
+for i in range(0, spl_num):
+    plt.plot(regressed_x[i], proc_spl_ydata[i], 'o', label=i+1)
+    plt.plot(x_fit, y_fit1[i],  label = i+1)
+
+plt.plot(x_fit, y_fit,label = '4pl_curve_fit')
+
+#print(popt)
+print(popt1)
+print(popt2)
+print(popt3)
+
+plt.legend()
+plt.xscale('log')
+plt.show()
+
+
+#########################################
+residuals1 = []
+
+residuals1.append(proc_spl_ydata[0] - logistic4(proc_spl_xdata[0], *popt1))
+residuals1.append(proc_spl_ydata[1] - logistic4(proc_spl_xdata[1], *popt2))
+residuals1.append(proc_spl_ydata[2] - logistic4(proc_spl_xdata[2], *popt3))
+
+ss_res1 = []
+
+ss_res1.append(np.sum(residuals1[0]**2))
+ss_res1.append(np.sum(residuals1[1]**2))
+ss_res1.append(np.sum(residuals1[2]**2))
+
+ss_tot1 = []
+ss_tot1.append(np.sum((proc_spl_ydata[0]-np.mean(proc_spl_ydata[0]))**2))
+ss_tot1.append(np.sum((proc_spl_ydata[1]-np.mean(proc_spl_ydata[1]))**2))
+ss_tot1.append(np.sum((proc_spl_ydata[2]-np.mean(proc_spl_ydata[2]))**2))
+
+r_squared1 = []
+r_squared1.append(1 - (ss_res1[0]/ss_tot1[0]))
+r_squared1.append(1 - (ss_res1[1]/ss_tot1[1]))
+r_squared1.append(1 - (ss_res1[2]/ss_tot1[2]))
+
+print(r_squared1)
--- Robot_validation/valid_test.py
+++ Robot_validation/valid_test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from math import sqrt
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('./Robot_validation/robot3_DO0.csv')
 #print(df)
@@ -58,76 +60,181 @@ p1_df = df.loc[:, column_name]
 
 # data processing which actual digital output bit equal to 1
 p2_df = p1_df[df['actual_digital_output_bits']==1]
-#p2_df = p2_df.reset_index(drop=True)
-#print(p2_df)
+
+# 데이터 프로세싱 - 1에서 0되는 부분 찾아야됨
+
+
+
 # shift to 1 below to all data
 p2_df_shift = p2_df.shift(periods=1, fill_value=0)
 
 # substract
 p3_df = p2_df - p2_df_shift
+#print(p3_df)
+
+df_1st = p1_df.shift(periods=1, fill_value=0)
+df_2nd = p1_df - df_1st
+
+#print(p3_df)
+df_3rd = df_2nd[df_2nd['actual_digital_output_bits']==-1]
 
 #print(p2_df)
-#print(p3_df)
+#print(df_3rd)
 
 # data processing - 차이가 0.02 이상인 부근의 데이터 프레임만 찾기
 p4_df = p3_df[p3_df['timestamp']>0.02]
 
+# data processing
+index_df_4th = df_3rd.index.tolist()
+print(index_df_4th)
+
+
 # 차이가 0.02이상의 부근인 데이터에 대한 인덱스 찾기
 index_p4_df = p4_df.index.tolist()
 #print(index_p4_df)
-#해당 인덱스에 대한 데이터 프레임
-a1 = p1_df.iloc[index_p4_df]
 
-a2 = p1_df.loc[4665:4681]
+#해당 인덱스에 대한 데이터 프레임, 300 equals to 30s
+a1 = p1_df.iloc[index_p4_df[0]:index_p4_df[0]+300]
+a2 = p1_df.iloc[index_p4_df[1]:index_p4_df[1]+300]
+#print(a1)
+
+a3 = df.iloc[index_df_4th[0]:index_df_4th[0]+199]
+a4 = df.iloc[index_df_4th[0]:index_df_4th[0]+199]
+
+#########################################################
+
+# 해당 인덱스부터 신호 기간 동안만 데이터를 추출하기
+a = {}
+
+for i in range(0, len(index_p4_df)):
+    a[i]=p1_df.iloc[index_p4_df[i]:index_p4_df[i]+300]
+
+dis_x_cord = {}
+dis_y_cord = {}
+dis_z_cord = {}
+
+for i in range(0, len(index_p4_df)):
+    dis_x_cord[i]= np.array(a[i]['target_TCP_pose_0']-a[i]['actual_TCP_pose_0'])*1000000
+    dis_y_cord[i]= np.array(a[i]['target_TCP_pose_1']-a[i]['actual_TCP_pose_1'])*1000000
+    dis_z_cord[i]= np.array(a[i]['target_TCP_pose_2']-a[i]['actual_TCP_pose_2'])*1000000
+
+######################################################################
+
+bc = {}
+
+for i in range(0, len(index_df_4th)):
+    bc[i]=df.iloc[index_df_4th[i]:index_df_4th[i]+199]
+
+spd_cord = {}
+
+for i in range(0, len(index_df_4th)):
+    spd_cord[i]= np.square(np.array(bc[i]['actual_TCP_speed_1']))+\
+    np.square(np.array(bc[i]['actual_TCP_speed_0']))+np.square(np.array(bc[i]['actual_TCP_speed_2']))
+    spd_cord[i] = np.sqrt(spd_cord[i]*1000000)
+
+print(spd_cord)
+
+spd_target_cord={}
+
+for i in range(0, len(index_df_4th)):
+    spd_target_cord[i]= np.square(np.array(bc[i]['target_TCP_speed_1']))+\
+    np.square(np.array(bc[i]['target_TCP_speed_0']))+np.square(np.array(bc[i]['target_TCP_speed_2']))
+    spd_target_cord[i] = np.sqrt(spd_target_cord[i]*1000000)
 
 
-#print(a2)
+
+values12 = list(spd_target_cord.values())
 
 
-x_cord = np.array(a2['target_TCP_pose_0']-a2['actual_TCP_pose_0'])*1000000
-y_cord = np.array(a2['target_TCP_pose_1']-a2['actual_TCP_pose_1'])*1000000
-z_cord = np.array(a2['target_TCP_pose_2']-a2['actual_TCP_pose_2'])*1000000
 
-#print(a2['actual_TCP_pose_3'])
-a3 = a2['actual_TCP_pose_3']
-print(a3)
+#####################################################################
 
 
-if a3> 3.12414 and a3 < 3.15905:
-    a2['actual_TCP_pose_3'] = a2['actual_TCP_pose_3'] 
+dis_targetbtnreal = {}
 
-if a2['actual_TCP_pose_3'] < -3.12414 and a2['actual_TCP_pose_3'] > -3.15905:
-    a2['actual_TCP_pose_3'] = a2['actual_TCP_pose_3'] + 6.28319
+for i in range(0, len(index_p4_df)):
+    dis_targetbtnreal[i] = np.sqrt(np.square(dis_x_cord[i])+np.square(dis_y_cord[i])+np.square(dis_z_cord[i]))
 
-print(a2['actual_TCP_pose_3'])
+keys = list(dis_targetbtnreal.keys())
+values = list(dis_targetbtnreal.values())
+
+values_std = []
+values_std.append(np.std(values[0]))
+values_std.append(np.std(values[1]))
+
+values_mean = []
+values_mean.append(np.mean(values[0]))
+values_mean.append(np.mean(values[1]))
+
+xxdata = []
+
+###############################################################
 
 
-#a2['actual_TCP_pose_4'] = correction_radian(a2['actual_TCP_pose_4'])
-#a2['actual_TCP_pose_5'] = correction_radian(a2['actual_TCP_pose_5'])
+keys1 = list(spd_cord.keys())
+values1 = list(spd_cord.values())
 
-rx1 = np.array(a2['target_TCP_pose_3']-a2['actual_TCP_pose_3'])
-ry1 = np.array(a2['target_TCP_pose_4']-a2['actual_TCP_pose_4'])
-rz1 = np.array(a2['target_TCP_pose_5']-a2['actual_TCP_pose_5'])
+values_std1 = []
+values_std1.append(np.std(values1[0]))
+values_std1.append(np.std(values1[1]))
 
-print(rx1)
-print(ry1)
-print(rz1)
+values_mean1 = []
+values_mean1.append(np.mean(values1[0]))
+values_mean1.append(np.mean(values1[1]))
 
-for i in range(0, len(rx1)):
+xxdata = []
 
-    if rx1[i] < 0.001 :
-        rx1[i] = 0
-    if ry1[i] < 0.001:
-        ry1[i] = 0    
-    if rz1[i] < 0.001:
-        rz1[i] = 0    
+
+#############################################################
+
+
+
+for i in range(0, len(index_p4_df)):
+    xxdata.append(np.full(1, i))
+    print(xxdata)
+    plt.bar(xxdata[i], values_mean[i], yerr=values_std[i], label='Move'+str(i+1))
+
+plt.legend()
+plt.ylim((0, 70))
+plt.show()
+
+timestamp_signal1 = np.arange(0, 30, 0.1)
+for i in range(0, len(index_p4_df)):
+
+    plt.plot(timestamp_signal1, values[i],label='Move'+str(i+1))
+
+plt.legend()
+plt.ylim((0,150))
+plt.show()
+
+################################################
+
+timestamp_signal2 = np.arange(0, 19.9, 0.1)
+
+plt.plot(timestamp_signal2, values12[0], label="Target Speed")
+for i in range(0, len(index_df_4th)):
+    
+    plt.plot(timestamp_signal2, values1[i], label='Move'+str(i+1))
+
+plt.legend()
+plt.show()
+
+
+
+####################################################
+# 각도에 대한 부분은 추후에 진행할 것
+#################################################
+
+#rx1 = np.array(a2['target_TCP_pose_3']-a2['actual_TCP_pose_3'])
+#ry1 = np.array(a2['target_TCP_pose_4']-a2['actual_TCP_pose_4'])
+#rz1 = np.array(a2['target_TCP_pose_5']-a2['actual_TCP_pose_5'])
 
 #print(rx1)
 #print(ry1)
 #print(rz1)
-deviation = []
-for i in range(0, len(x_cord)):
-    
-    deviation.append(sqrt(x_cord[i]*x_cord[i] + y_cord[i]*y_cord[i]))
 
-#print(deviation)
+######################################################
+hue1 = np.diff(values1[0])
+hue1 = pd.Series(hue1)
+plt.plot(timestamp_signal2[:-1],hue1.rolling(window=15, center=True).mean()*10)
+plt.show()
